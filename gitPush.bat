4bcbed00240832afa6ee23bef6cd49234eaba441
SmilingNod <menes_13@hotmail.fr>
2015-05-09 15:07:14 +0300

    Maj Dossier Antoine

---
+++ gitPush.bat
@@ -0,0 +1,14 @@
+
+:start
+cd C:\Users\AM-enssat\Documents\Turtle_Salad
+
+C:\Users\AM-enssat\AppData\Local\GitHub\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad\bin\git.exe commit -am "made zero change"
+
+C:\Users\AM-enssat\AppData\Local\GitHub\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad\bin\git.exe push
+
+
+echo . >> monfichier
+
+ping 192.0.2.2 -n 1 -w 10000 > nul
+
+goto start
